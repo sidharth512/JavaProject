17a5b468001a52d182ff3bee059a6908b512844f
sidharth512 <papulu.pattanaik@gmail.com>
2022-09-04 11:50:30 +0530

    First Commit

---
+++ src/Twst.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Twst {
+    public static void main(String[] arg) {
+        String s = 10 + 20 + 30 + 40 + " Java";
+        //100"java"
+        String s1 = 20 + 30 + "java" + 10 + 40;
+        //100"java"
+        String s2 = "Java" + 20 + 30 + 10;
+        //"java"60
+
+    }
+}
